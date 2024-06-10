e8879e926879e04a7dbef24e178339763572db23
mamurjon1148 <mamurjonovmamurjon01@gmail.com>
2024-06-10 21:25:02 +0500

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+let letterSearch = ["Non", "Asal", "Banan", "Olma", "Aziza", "Gul", "Moshina"];
+let nBor = []
+let nYoq = []
+
+
+for (let narsa of letterSearch) {
+    if (narsa.includes("n")) {
+        nBor.push(narsa))
+    }
+    
+    else {
+        nYoq.push(narsa)
+    }
+}
+
+console.log(nBor);
+console.log(nYoq);
