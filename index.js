cc21a6adff9ad146a3eb54542b1b7782abcc5e68
mamurjon1148 <mamurjonovmamurjon01@gmail.com>
2024-06-11 15:10:32 +0500

    done

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let letterSearch = ["Non", "Asal", "Banan", "Olma", "Aziza", "Gul", "Moshina"];
+let products = ["Non", "Asal", "Banan", "Olma", "Aziza", "Gul", "Moshina"];
 let nBor = []
 let nYoq = []
 
-
-for (let narsa of letterSearch) {
-    if (narsa.includes("n")) {
-        nBor.push(narsa))
+for (let product of products) {
+    if (product.includes("n")) {
+        nBor.push(narsa)
     }
     
     else {
